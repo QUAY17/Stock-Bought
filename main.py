df2c41353156e93dea09cc8bf3ffcfcf562d3d0c
QUAY17 <jminniecc@gmail.com>
2022-04-06 12:11:10 -0600

    adding DMA strategy

--- main.py
+++ main.py
@@ -5,15 +5,15 @@
 * date: Spring 2022
 * Toolkit to extend the Robinhood Investment API
 """
+
 import backtrader as bt
 import yfinance as yf
 from datetime import datetime
 import matplotlib as plt
-
+import pandas as pd
 
 """
-* creates trading strategy
-* Donchian Strategy
+* creates Donchian Strategy
 * used to buy breakouts
 * buy when the price breaks through the 20-period high and
 * sell when the price drops below the 20-period low
@@ -21,7 +21,7 @@ import matplotlib as plt
 
 class DonchianStrategy(bt.Strategy):
     def __init__(self):
-        self.myind = DonchianChannels()
+        self.myind = Donchian()
 
     def next(self):
         if self.data[0] > self.myind.dch[0]:
@@ -29,7 +29,7 @@ class DonchianStrategy(bt.Strategy):
         elif self.data[0] < self.myind.dcl[0]:
             self.sell()
 
-class DonchianChannels(bt.Indicator):
+class Donchian(bt.Indicator):
     '''
     Params Note:
       - `lookback` (default: -1)
@@ -64,15 +64,66 @@ class DonchianChannels(bt.Indicator):
         self.l.dcl = bt.ind.Lowest(lo, period=self.p.period)
         self.l.dcm = (self.l.dch + self.l.dcl) / 2.0  # avg of the above
 
+
 """
-* creates trading strategy
-* Dual Moving Average (DMA) Strategy
+* creates 200 day Simple Moving Average (SMA) Strategy
+* determines the long term general market trend
 """
+class SMA200Strategy(bt.Strategy):
+
+    def __init__(self):  # Initiation
+        self.sma = bt.ind.SimpleMovingAverage()  # Processing
+
+"""
+* creates Average True Range (ATR) Strategy
+* give an estimate of fluctuation in a given period
+* iterates through the last 14 data points 
+* takes the high and subtract the low for each period then averages it out
+"""
+class AverageTrueRange(bt.Strategy):
+
+	def log(self, txt, dt=None):
+		dt = dt or self.datas[0].datetime.date(0)
+		print(f'{dt.isoformat()} {txt}') #Print date and close
+		
+	def __init__(self):
+		self.dataclose = self.datas[0].close
+		self.datahigh = self.datas[0].high
+		self.datalow = self.datas[0].low
+		
+	def next(self):
+		range_total = 0
+		for i in range(-13, 1):
+			true_range = self.datahigh[i] - self.datalow[i]
+			range_total += true_range
+		ATR = range_total / 14
+
+		self.log(f'Close: {self.dataclose[0]:.2f}, ATR: {ATR:.4f}')
+
+"""
+* creates 50 day Simple Moving Average (SMA) Strategy
+* determines the short term general market trend
+* use the start and end date wrapper
+"""
+class SMA50Strategy(bt.SignalStrategy):
+    def __init__(self):  # Initiation
+        self.sma = bt.ind.SimpleMovingAverage(period=50)  # Processing
+    
+    def next(self):  # Processing
+        if self.sma > self.data.close:
+            self.buy()
+        elif self.sma < self.data.close: # Post-processing
+            self.close()
+
+"""
+* creates Dual Moving Average (DMA) Strategy
+* SIGNAL_LONG is trading logic
+"""        
 class DMAStrategy(bt.SignalStrategy):
     def __init__(self):
-        sma1, sma2 = bt.ind.SMA(period=10), bt.ind.SMA(period=20)
-        crossover = bt.ind.CrossOver(sma1, sma2)
-        self.signal_add(bt.SIGNAL_LONG, crossover)
+        short, long = bt.ind.SMA(period=50), bt.ind.SMA(period=200)
+        crossover = bt.ind.CrossOver(short, long)
+        self.signal_add(bt.SIGNAL_LONG, crossover) #this is the trading logic
 
 """
 * backtest function
@@ -93,12 +144,12 @@ def backtest(strategy, ticker, fromdate, todate, cash, commission=0.00):
     data = bt.feeds.PandasData(dataname=yf.download(ticker, fromdate, todate))
     cerebro.adddata(data)
 
-    # run the backtest!
+    # show portfolio stats before and after runnign the backtest
     print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
     cerebro.run()
     print('Ending Portfolio Value: %.2f' % cerebro.broker.getvalue())
 
-    # plot it, the extra plt commands for getting it inline, default doesn't work in colab
+    # plot it
     plt.rcParams['figure.figsize'] = [15, 12]
     plt.rcParams.update({'font.size': 12}) 
     cerebro.plot(iplot=False)
@@ -107,5 +158,4 @@ def backtest(strategy, ticker, fromdate, todate, cash, commission=0.00):
 if __name__ == '__main__':
 
     # backtest
-    # call robinhood api 
-    backtest(strategy=DonchianStrategy, ticker='TSLA', fromdate='2017-01-01', todate='2020-12-31', cash=1000.0)
+    backtest(strategy=DMAStrategy, ticker='TSLA', fromdate='2018-01-01', todate='2022-04-01', cash=1000.0)
