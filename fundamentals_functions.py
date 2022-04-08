970dae34380561818dff7f8aa9fe4dc4f127c897
Brian Kirk <bmarshallk@gmail.com>
2022-04-08 11:36:44 -0600

    Update fundamentals_functions.py
    Returns values instead of print statements

--- fundamentals_functions.py
+++ fundamentals_functions.py
@@ -5,13 +5,11 @@ def get_margins(ticker):
 	'''returns margin percentages for a given company'''
 	data = yf.Ticker(ticker)
 	margin_keys = ['profitMargins', 'grossMargins', 'operatingMargins']
-	for key in margin_keys:
-		print("%s: %s%%" % (key, str(round(100 * data.info[key], 1))))
+	return data.info[margin_keys[0]], data.info[margin_keys[1]], data.info[margin_keys[2]]
 
 
 def get_forwards(ticker):
 	'''returns forward EPS and PE'''
 	data = yf.Ticker(ticker)
 	forward_keys = ['forwardEps', 'forwardPE']
-	for key in forward_keys:
-		print('%s: %s' % (key, round(data.info[key], 2)))
+	return data.info[forward_keys[0]], data.info[forward_keys[1]]	
