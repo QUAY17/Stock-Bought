44a3f6068d8a8985c2a21a0b440bd37dc98d62eb
QUAY17 <jminniecc@gmail.com>
2022-04-18 13:03:38 -0600

    Added only the strategies we can use with our robin stocks trading logic, added robin stocks login

---
+++ backtesting.py
@@ -0,0 +1,123 @@
+"""
+* CSE326 Stock Bought 
+* file: main.py
+* authors: Jennifer Minnich
+* date: Spring 2022
+* Toolkit to extend the Robinhood Investment API
+"""
+
+import backtrader as bt
+import yfinance as yf
+from datetime import datetime
+import matplotlib as plt
+import pandas as pd
+from time import sleep
+import robin_stocks as r
+import robin_stocks.robinhood as r
+     
+"""
+* creates 200 day Simple Moving Average (SMA) Strategy
+* determines the long term general market trend
+"""
+class SMA200Strategy(bt.Strategy):
+
+    def __init__(self):  # Initiation
+        self.sma = bt.ind.SimpleMovingAverage()  # Processing
+
+
+"""
+* creates 50 day Simple Moving Average (SMA) Strategy
+* determines the short term general market trend
+* use the start and end date wrapper
+"""
+class SMA50Strategy(bt.SignalStrategy):
+    def __init__(self):  # Initiation
+        self.sma = bt.ind.SimpleMovingAverage(period=50)  # Processing
+
+    def next(self):  # Processing
+        if self.sma > self.data.close:
+            self.buy()
+        elif self.sma < self.data.close: # Post-processing
+            self.close()
+
+class SMA50StrategyLive(bt.SignalStrategy):
+    def __init__(self):  # Initiation
+        #if live_trading, close is the last data point from robin stocks
+        price = r.get_latest_price('TSLA')
+        close = price[0]
+        print(close)
+        
+        self.sma = bt.ind.SimpleMovingAverage(period=50)  # Processing      
+    
+    def next(self):  # Processing
+        if self.sma > self.data.close:
+            #robinstocks functions
+            self.buy()
+        elif self.sma < self.data.close: # Post-processing
+            self.close()
+
+#robinhood login
+def login():
+    r.login("jminniecc@gmail.com","hgPSznGL5STp8QK")
+
+"""
+* backtest function
+* uses strategy selection from user
+* ticker input from user
+* cash input by user
+* commission set to 0
+* adding the sizers will scale trades bt.sizers, possible add-on as features
+"""
+def backtest(strategy, ticker, fromdate, todate, cash, commission=0.00):
+
+    # initialize the engine with your strategy, cash amount, etc
+    cerebro = bt.Cerebro()
+    cerebro.addstrategy(strategy)
+    cerebro.broker.setcash(cash)
+    cerebro.broker.setcommission(commission=commission)
+
+    # pulling the data to put into the engine
+    data = bt.feeds.PandasData(dataname=yf.download(ticker, fromdate, todate))
+    cerebro.adddata(data)
+    #cerebro.addsizer(bt.sizers.AllInSizer)
+    
+    # show portfolio stats before and after runnign the backtest
+    print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
+    cerebro.run()
+    print('Ending Portfolio Value: %.2f' % cerebro.broker.getvalue())
+
+    # plot it
+    plt.rcParams['figure.figsize'] = [15, 12]
+    plt.rcParams.update({'font.size': 12}) 
+    #cerebro.plot()
+    #saves plot as png 
+    figure = cerebro.plot(iplot=False)[0][0]
+    figure.savefig('backtest_plot.png')
+
+
+def live_trade(ticker):
+
+    #call robinhood login
+    login()
+
+    price = r.get_latest_price(ticker)
+    close = price[0]
+    print("updated price", close)
+
+    cerebro = bt.Cerebro()
+    
+    SMA50StrategyLive(bt.SignalStrategy)
+
+
+if __name__ == '__main__':
+
+    # backtest
+    # todate +1 day for most recent closing price
+    backtest(strategy=SMA50Strategy, ticker='TSLA', fromdate='2018-01-01', todate='2022-04-19', cash=1000.0)
+
+    live_trading = input("Live market data? Y/N")
+    if live_trading == 'Y':
+        live_trade(ticker='TSLA')
+        
+
+
