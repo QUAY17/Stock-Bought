244a1815e9345c7829a6b76007252b8721bd98df
Brian <bmarshallk@gmail.com>
2022-04-20 20:52:37 -0600

    Updated to class for threading

--- live_algorithms.py
+++ live_algorithms.py
@@ -3,48 +3,56 @@ import queue
 import robin_stocks
 import robin_stocks.robinhood as r
 
-def kill_switch(bool):
-    if bool is True:
-        kill_signal = True
-    else:
-        kill_signal = False
+class sma50hr():
 
-    return kill_signal
+    def __init__(self, ticker):
 
-def sma50hr(ticker, kill_signal):
-    '''do some trading based around the 50-hour moving average'''
+        # initialize
+        self.ticker = ticker
+        self.kill_signal = False
 
-    # get data to create the moving average:
-    data = robin_stocks.robinhood.stocks.get_stock_historicals(ticker, interval='hour', span='week')
-    prices = []
-    for datum in data:
-        prices.append(float(datum['close_price']))
-        
-    # Define a queue 50 points long for the moving average and fill it with the latest data
-    baseline = queue.Queue(50)
-    for price in prices[-50:]:
-        baseline.put(price)
+        # Get data to create the moving average
+        data = robin_stocks.robinhood.stocks.get_stock_historicals(self.ticker, interval='hour', span='week')
+        prices = []
+        for datum in data:
+            prices.append(float(datum['close_price']))
+            
+        # Define a queue 50 points long for the moving average and fill it with the latest data
+        self.baseline = queue.Queue(50)
+        for price in prices[-50:]:
+            self.baseline.put(price)
 
-    while kill_signal is False:
 
-        # calculate the moving average, can't do this directly on queue's so I dump it to a list to calculate
-        moving_average = round(sum(list(baseline.queue)) / len(list(baseline.queue)), 2)
-        print('Moving average', moving_average)
+    def start_trading(self):
 
-        # get the current spot price
-        price = round(float(r.get_latest_price(ticker)[0]))
-        print('Current price:', price)
+        # Live trade until the kill signal is sent
+        while self.kill_signal is False:
 
-        # trading logic:
-        if price <= moving_average:
-            print("buy")
-        else:
-            print("sell")
+            # calculate the moving average, can't do this directly on queue's so I dump it to a list to calculate
+            self.moving_average = round(sum(list(self.baseline.queue)) / len(list(self.baseline.queue)), 2)
+            print('Moving average', self.moving_average)
 
-        # update the moving average with the recent datapoint and recompute the moving average
-        # first remove the first point put in initially to make room using .get()
-        baseline.get()
-        baseline.put(price)
+            # get the current spot price
+            price = round(float(r.get_latest_price(ticker)[0]))
+            print('Current price:', price)
 
-        # wait a few seconds before pulling prices again
-        sleep(10)
+            # trading logic:
+            if price <= moving_average:
+                print("Bought $1 of %s" % ticker)
+                #rorder_buy_fractional_by_price(ticker, amountInDollars=1)
+
+            else:
+                print("Sold $1 of %s" % ticker)
+                #r.order_sell_fractional_by_price(ticker, amountInDollars=1)
+
+            # update the moving average with the recent datapoint and recompute the moving average
+            # first remove the first point put in initially to make room using .get()
+            self.baseline.get()
+            self.baseline.put(price)
+
+            # wait a few seconds before pulling prices again
+            sleep(10)
+
+
+    def return_details(self):
+        return self.moving_average 
