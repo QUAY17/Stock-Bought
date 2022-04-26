eb81272e4b690a7106432383a89d86abef455760
Brian <bmarshallk@gmail.com>
2022-04-25 18:18:18 -0600

    Commenting out until usable

--- test_fed_functions.py
+++ test_fed_functions.py
@@ -2,7 +2,7 @@ import unittest
 import fed_functions as ff
 
 class TestFedFunctions(unittest.TestCase):
-    def test_API(self):
+    #def test_API(self):
         # Tests the API key is valid (this function doesn't return anything?)
         #self.assertEquals(ff.load_api_key(), '59b76a3e8c24982778e2ae0c69a92996')
 
