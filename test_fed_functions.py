ccc3dc186a6e1cb8b2e5d3b15282be214c94dd3c
Brian <bmarshallk@gmail.com>
2022-04-26 15:52:00 -0600

    Updated functions tests

--- test_fed_functions.py
+++ test_fed_functions.py
@@ -2,8 +2,8 @@ import unittest
 import fed_functions as ff
 
 class TestFedFunctions(unittest.TestCase):
-    #def test_API(self):
-        # Tests the API key is valid (this function doesn't return anything?)
-        #self.assertEquals(ff.load_api_key(), '59b76a3e8c24982778e2ae0c69a92996')
-
-    # Most functions in there are hardcoded? 
+    def test_type_get_interest_rate(self):
+        # Tests the length and type of the output
+        actual = type(ff.get_overnight_rate())
+        expected = float
+        self.assertEquals(actual, expected)
